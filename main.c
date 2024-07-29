06270c9dbda0fce952f924af1bcf0b45fd1f1164
akinnihat <nihatakin2@gmail.com>
2024-07-29 23:03:31 +0300

    created asteroids

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <math.h>
- 
+
 #define M_PI 3.14159265358979323846
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
@@ -32,8 +33,8 @@ typedef struct
 typedef struct
 {
     SDL_Point coordinates;
-    int radius;
-    int life;
+    int diameter;
+    int speed;
 } Asteroid;
 
 // typedef struct {
@@ -62,6 +63,79 @@ void handle_player_input(const Uint8* player_input, Player* player)
             player->coordinates.y += player->speed * sin(player->angle * M_PI / 180.0);
             player->speed -= 1.0f; 
         }
+    }
+}
+
+void draw_circle(SDL_Renderer* renderer, int32_t center_x, int32_t center_y, int32_t radius)
+{
+   const int32_t diameter = (radius * 2);
+
+   int32_t x = (radius - 1);
+   int32_t y = 0;
+   int32_t tx = 1;
+   int32_t ty = 1;
+   int32_t error = (tx - diameter);
+
+   while (x >= y)
+   {
+        SDL_RenderDrawPoint(renderer, center_x + x, center_y - y);
+        SDL_RenderDrawPoint(renderer, center_x + x, center_y + y);
+        SDL_RenderDrawPoint(renderer, center_x - x, center_y - y);
+        SDL_RenderDrawPoint(renderer, center_x - x, center_y + y);
+        SDL_RenderDrawPoint(renderer, center_x + y, center_y - x);
+        SDL_RenderDrawPoint(renderer, center_x + y, center_y + x);
+        SDL_RenderDrawPoint(renderer, center_x - y, center_y - x);
+        SDL_RenderDrawPoint(renderer, center_x - y, center_y + x);
+
+        if (error <= 0)
+        {
+            ++y;
+            error += ty;
+            ty += 2;
+        }
+
+        if (error > 0)
+        {
+            --x;
+            tx += 2;
+            error += (tx - diameter);
+        }
+    }
+}
+
+void draw_asteroids(SDL_Renderer* renderer, Asteroid* asteroids[], int32_t asteroid_count)
+{
+    // TO BE UPDATED
+    int32_t x = ((asteroids[0]->diameter)/2 - 1);
+    int32_t y = 0;
+    int32_t tx = 1;
+    int32_t ty = 1;
+    int32_t error = (tx - asteroids[0]->diameter);
+
+    while (x >= y)
+    {
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + x, &asteroids[0]->coordinates.y - y);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + x, &asteroids[0]->coordinates.y + y);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - x, &asteroids[0]->coordinates.y - y);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - x, &asteroids[0]->coordinates.y + y);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + y, &asteroids[0]->coordinates.y - x);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + y, &asteroids[0]->coordinates.y + x);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - y, &asteroids[0]->coordinates.y - x);
+       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - y, &asteroids[0]->coordinates.y + x);
+
+       if (error <= 0)
+       {
+          ++y;
+          error += ty;
+          ty += 2;
+       }
+
+       if (error > 0)
+       {
+          --x;
+          tx += 2;
+          error += (tx - asteroids[0]->diameter);
+       }
     }
 }
 
@@ -96,7 +170,7 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    SDL_Texture* asteroid_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 129, 129);
+    SDL_Texture* asteroid_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
     if (asteroid_texture == NULL) {
         printf("Unable to create texture! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyRenderer(renderer);
@@ -105,12 +179,36 @@ int main(int argc, char ** argv)
         return 1;
     }
 
+    //SDL_SetRenderDrawBlendMode(player.texture, SDL_BLENDMODE_BLEND);
+    //SDL_SetRenderDrawBlendMode(asteroid_texture, SDL_BLENDMODE_BLEND);
+
     SDL_SetRenderTarget(renderer, player.texture);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
     SDL_RenderDrawLines(renderer, player.points, 5);
     SDL_SetRenderTarget(renderer, NULL);
+
+    Asteroid asteroids[64];
+    int asteroid_count = 0;
+
+    while (asteroid_count < 64)
+    {
+        srand(time(NULL));
+        int random_diameter = rand() % 20 + 10;
+        srand(time(NULL));
+        int random_x = rand() % SCREEN_WIDTH;
+        srand(time(NULL));
+        int random_y = rand() % SCREEN_HEIGHT;
+
+        Asteroid asteroid = { .coordinates.x = random_x, .coordinates.y = random_y,
+                                .diameter = random_diameter, .speed = 5};
+        asteroids[asteroid_count] = asteroid; 
+        asteroid_count += 1;
+        printf("%d\n", asteroid_count);
+    }
+
+    printf("%d\n", asteroids[21].coordinates.x);
 
     while (!quit)
     {   
@@ -122,6 +220,15 @@ int main(int argc, char ** argv)
             break;   
         }
 
+        SDL_SetRenderTarget(renderer, asteroid_texture);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        //SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        for (int i = 0; i < asteroid_count; i++) { draw_circle(renderer, asteroids[i].coordinates.x, asteroids[i].coordinates.y, asteroids[i].diameter); }
+        //draw_asteroids(renderer, &asteroids, asteroid_count);
+        SDL_SetRenderTarget(renderer, NULL);
+
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
@@ -129,6 +236,8 @@ int main(int argc, char ** argv)
         handle_player_input(player_input, &player);
 
         SDL_Rect player_dst_rect = {.x = player.coordinates.x, .y = player.coordinates.y, .w = 65, .h = 65};
+        SDL_Rect aster_dst_rect = {.x = 100, .y = 100, .w = SCREEN_WIDTH, .h = SCREEN_HEIGHT};
+        SDL_RenderCopyEx(renderer, asteroid_texture, NULL, &aster_dst_rect, 0, NULL, SDL_FLIP_NONE);
         SDL_RenderCopyEx(renderer, player.texture, NULL, &player_dst_rect, player.angle+90, NULL, SDL_FLIP_NONE);
         
         SDL_RenderPresent(renderer);
@@ -136,6 +245,7 @@ int main(int argc, char ** argv)
  
     // Cleanup SDL
     SDL_DestroyTexture(player.texture);
+    SDL_DestroyTexture(asteroid_texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
