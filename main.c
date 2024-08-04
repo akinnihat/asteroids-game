b159eed8cedce548a4bdf24018928c355b3ba610
akinnihat <nihatakin2@gmail.com>
2024-08-04 18:27:16 +0300

    collision detection and score update

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <math.h>
 #include <time.h>
+
 
 #define M_PI 3.14159265358979323846
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+#define ASTEROID_COUNT 8
 
 SDL_Color white = {255, 255, 255};
 
@@ -19,12 +20,6 @@ enum PlayerPoints
     POINT_3,
     POINT_END
 };
-
-// typedef enum 
-// {
-//     TYPE_PLAYER,
-//     TYPE_ASTEROID
-// } ObjectType;
 
 typedef struct 
 {
@@ -42,20 +37,13 @@ typedef struct
     int direction[2];
 } Asteroid;
 
-// typedef struct {
-//     void* data;
-//     ObjectType type;
-// } GameObject;
 
 void handle_player_input(const Uint8* player_input, Player* player)
 {
-    if (player_input[SDL_SCANCODE_LEFT]) {
-        player->angle -= 5;
-    }
-    if (player_input[SDL_SCANCODE_RIGHT]) {
-        player->angle += 5;
-    }
-    if (player_input[SDL_SCANCODE_UP]) {
+    if (player_input[SDL_SCANCODE_LEFT]) { player->angle -= 5; }
+    if (player_input[SDL_SCANCODE_RIGHT]) { player->angle += 5; }
+    if (player_input[SDL_SCANCODE_UP]) 
+    {
         if (player->speed < 6.0f) { player->speed += 0.2f; } 
         player->coordinates.x += player->speed * cos(player->angle * M_PI / 180.0);
         player->coordinates.y += player->speed * sin(player->angle * M_PI / 180.0);
@@ -75,26 +63,26 @@ void handle_player_input(const Uint8* player_input, Player* player)
     else if (player->coordinates.y <= 0) { player->coordinates.y = player->coordinates.y + SCREEN_HEIGHT; }
 }
 
-void draw_circle(SDL_Renderer* renderer, int32_t center_x, int32_t center_y, int32_t radius)
+void draw_asteroid(SDL_Renderer* renderer, Asteroid asteroid)
 {
-   const int32_t diameter = (radius * 2);
+    int32_t x = ((asteroid.diameter)/2 - 1);
+    int32_t y = 0;
+    int32_t tx = 1;
+    int32_t ty = 1;
+    int32_t error = (tx - asteroid.diameter);
 
-   int32_t x = (radius - 1);
-   int32_t y = 0;
-   int32_t tx = 1;
-   int32_t ty = 1;
-   int32_t error = (tx - diameter);
-
-   while (x >= y)
-   {
-        SDL_RenderDrawPoint(renderer, center_x + x, center_y - y);
-        SDL_RenderDrawPoint(renderer, center_x + x, center_y + y);
-        SDL_RenderDrawPoint(renderer, center_x - x, center_y - y);
-        SDL_RenderDrawPoint(renderer, center_x - x, center_y + y);
-        SDL_RenderDrawPoint(renderer, center_x + y, center_y - x);
-        SDL_RenderDrawPoint(renderer, center_x + y, center_y + x);
-        SDL_RenderDrawPoint(renderer, center_x - y, center_y - x);
-        SDL_RenderDrawPoint(renderer, center_x - y, center_y + x);
+    while (x >= y)
+    {
+        int asteroid_x = asteroid.coordinates.x; 
+        int asteroid_y = asteroid.coordinates.y;
+        SDL_RenderDrawPoint(renderer, asteroid_x + x, asteroid_y - y);
+        SDL_RenderDrawPoint(renderer, asteroid_x + x, asteroid_y + y);
+        SDL_RenderDrawPoint(renderer, asteroid_x - x, asteroid_y - y);
+        SDL_RenderDrawPoint(renderer, asteroid_x - x, asteroid_y + y);
+        SDL_RenderDrawPoint(renderer, asteroid_x + y, asteroid_y - x);
+        SDL_RenderDrawPoint(renderer, asteroid_x + y, asteroid_y + x);
+        SDL_RenderDrawPoint(renderer, asteroid_x - y, asteroid_y - x);
+        SDL_RenderDrawPoint(renderer, asteroid_x - y, asteroid_y + x);
 
         if (error <= 0)
         {
@@ -107,44 +95,8 @@ void draw_circle(SDL_Renderer* renderer, int32_t center_x, int32_t center_y, int
         {
             --x;
             tx += 2;
-            error += (tx - diameter);
+            error += (tx - asteroid.diameter);
         }
-    }
-}
-
-void draw_asteroids(SDL_Renderer* renderer, Asteroid* asteroids[], int32_t asteroid_count)
-{
-    // TO BE UPDATED
-    int32_t x = ((asteroids[0]->diameter)/2 - 1);
-    int32_t y = 0;
-    int32_t tx = 1;
-    int32_t ty = 1;
-    int32_t error = (tx - asteroids[0]->diameter);
-
-    while (x >= y)
-    {
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + x, &asteroids[0]->coordinates.y - y);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + x, &asteroids[0]->coordinates.y + y);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - x, &asteroids[0]->coordinates.y - y);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - x, &asteroids[0]->coordinates.y + y);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + y, &asteroids[0]->coordinates.y - x);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x + y, &asteroids[0]->coordinates.y + x);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - y, &asteroids[0]->coordinates.y - x);
-       SDL_RenderDrawPoint(renderer, &asteroids[0]->coordinates.x - y, &asteroids[0]->coordinates.y + x);
-
-       if (error <= 0)
-       {
-          ++y;
-          error += ty;
-          ty += 2;
-       }
-
-       if (error > 0)
-       {
-          --x;
-          tx += 2;
-          error += (tx - asteroids[0]->diameter);
-       }
     }
 }
 
@@ -158,14 +110,16 @@ void display_score(SDL_Renderer* renderer, TTF_Font* font, int score) {
         snprintf(result, sizeof(result), "%s", score_label, score_text);
 
         SDL_Surface* message_surface = TTF_RenderText_Solid(font, result, white); 
-        if (!message_surface) {
+        if (message_surface == NULL) 
+        {
             fprintf(stderr, "Could not create surface: %s\n", TTF_GetError());
             TTF_CloseFont(font);
             return;
         }
 
         SDL_Texture* message_texture = SDL_CreateTextureFromSurface(renderer, message_surface);
-        if (!message_texture) {
+        if (message_texture == NULL) 
+        {
             fprintf(stderr, "Could not create texture: %s\n", SDL_GetError());
             SDL_FreeSurface(message_texture);
             TTF_CloseFont(font);
@@ -182,10 +136,29 @@ void display_score(SDL_Renderer* renderer, TTF_Font* font, int score) {
     }
 }
 
-bool detect_collision(SDL_Point* player_coordinates, Asteroid* asteroids)
+// Bounding circle collision detection
+bool detect_collision(SDL_Rect* player_dst_rect, Asteroid asteroids[], int asteroid_count)
 {   
-    // To be added
-    printf("To be added.\n");
+    bool _is_collided = false;
+    int player_radius = (player_dst_rect->w/2)-7; // -7 for reduce collision area
+    for (int i = 0; i<asteroid_count; i++)
+    {
+        int asteroid_x = asteroids[i].coordinates.x;
+        int asteroid_y = asteroids[i].coordinates.y;
+        int asteroid_radius = asteroids[i].diameter;
+
+        int distance_x = player_dst_rect->x - asteroid_x;
+        int distance_y = player_dst_rect->y - asteroid_y;
+
+        int d = sqrt((distance_x * distance_x) + (distance_y * distance_y));
+
+        if (d <= (player_radius + asteroid_radius))
+        { 
+            _is_collided = true; 
+            break;
+        } else { _is_collided = false; }
+    }
+    return _is_collided;
 }
 
 int main(int argc, char ** argv)
@@ -201,23 +174,37 @@ int main(int argc, char ** argv)
                       .angle = 90, .speed = 6.0f };
 
     // Init SDL
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
+    if ((SDL_Init(SDL_INIT_VIDEO) < 0)) 
+    { 
+        printf("Could not initialize SDL: %s.\n", SDL_GetError());
+        exit(-1);
+    }
+
+    if (TTF_Init() == -1) 
+    { 
+        printf("Could not initialize TTF: %s.\n", SDL_GetError());
+        exit(-1);
+    }
+
     TTF_Font* font = TTF_OpenFont("Ubuntu-Light.ttf", 32);
-    if (!font) {
+    if (!font)
+    {
         fprintf(stderr, "Could not open font: %s\n", TTF_GetError());
         return;
     }
-    SDL_Window * window = SDL_CreateWindow("AsteroidsGame",
+
+    SDL_Window* window = SDL_CreateWindow("AsteroidsGame",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-        SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-    
-    // GameObject objects[] = {
-    //     { &player, TYPE_PLAYER },
-    // };
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if (window == NULL) 
+    { 
+        printf("Could not create window: %s.\n", SDL_GetError());
+        exit(-1);
+    }
 
     player.texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 65, 65);
-    if (player.texture == NULL) {
+    if (player.texture == NULL) 
+    {
         printf("Unable to create texture! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
@@ -226,16 +213,14 @@ int main(int argc, char ** argv)
     }
 
     SDL_Texture* asteroid_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
-    if (asteroid_texture == NULL) {
+    if (asteroid_texture == NULL) 
+    {
         printf("Unable to create texture! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
-
-    //SDL_SetRenderDrawBlendMode(player.texture, SDL_BLENDMODE_BLEND);
-    //SDL_SetRenderDrawBlendMode(asteroid_texture, SDL_BLENDMODE_BLEND);
 
     SDL_SetRenderTarget(renderer, player.texture);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
@@ -244,13 +229,13 @@ int main(int argc, char ** argv)
     SDL_RenderDrawLines(renderer, player.points, 5);
     SDL_SetRenderTarget(renderer, NULL);
 
-    Asteroid asteroids[8];
+    Asteroid asteroids[ASTEROID_COUNT];
     int asteroid_count = 0;
     
     srand(time(NULL));
-    while (asteroid_count < 8)
+    while (asteroid_count < ASTEROID_COUNT)
     {
-        int random_diameter = rand() % 20 + 30;
+        int random_diameter = (rand() % 20 + 30)*2;
         int random_x = rand() % SCREEN_WIDTH;
         int random_y = rand() % SCREEN_HEIGHT;
         int random_y_direction = 0;
@@ -279,13 +264,14 @@ int main(int argc, char ** argv)
             break;   
         }
 
+        // Draw asteroids
         SDL_SetRenderTarget(renderer, asteroid_texture);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
         for (int i = 0; i < asteroid_count; i++) 
         { 
-            draw_circle(renderer, asteroids[i].coordinates.x, asteroids[i].coordinates.y, asteroids[i].diameter); 
+            draw_asteroid(renderer, asteroids[i]); 
             asteroids[i].coordinates.x += asteroids[i].direction[0];  
             asteroids[i].coordinates.y += asteroids[i].direction[1];
             if (asteroids[i].coordinates.x > SCREEN_WIDTH) { asteroids[i].coordinates.x = asteroids[i].coordinates.x - SCREEN_WIDTH; }
@@ -293,7 +279,6 @@ int main(int argc, char ** argv)
             if (asteroids[i].coordinates.y > SCREEN_HEIGHT) { asteroids[i].coordinates.y = asteroids[i].coordinates.y - SCREEN_HEIGHT; }
             else if (asteroids[i].coordinates.y <= 0) { asteroids[i].coordinates.y = asteroids[i].coordinates.y + SCREEN_HEIGHT; }
         }
-        //draw_asteroids(renderer, &asteroids, asteroid_count);
         SDL_SetRenderTarget(renderer, NULL);
 
 
@@ -305,9 +290,9 @@ int main(int argc, char ** argv)
 
         SDL_Rect player_dst_rect = {.x = player.coordinates.x, .y = player.coordinates.y, .w = 65, .h = 65};
         SDL_Rect aster_dst_rect = {.x = 0, .y = 0, .w = SCREEN_WIDTH, .h = SCREEN_HEIGHT};
-        SDL_RenderCopyEx(renderer, asteroid_texture, NULL, &aster_dst_rect, 0, NULL, SDL_FLIP_NONE);
+        SDL_RenderCopy(renderer, asteroid_texture, NULL, &aster_dst_rect);
         SDL_RenderCopyEx(renderer, player.texture, NULL, &player_dst_rect, player.angle+90, NULL, SDL_FLIP_NONE);
-        
+
         Uint32 current_time = SDL_GetTicks();
         if (current_time - last_time >= 1000) 
         {
@@ -315,7 +300,9 @@ int main(int argc, char ** argv)
             last_time = current_time;
         }
         display_score(renderer, font, score);
-        //collided = detect_collision(&player.coordinates, asteroids);
+        
+        collided = detect_collision(&player_dst_rect, asteroids, asteroid_count);
+        if (collided) { score = 0; }
 
         SDL_RenderPresent(renderer);
     }
